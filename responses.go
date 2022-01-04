cd39940d5232cbdd5036092de974ed653718ddf4
Ante Wall <antewall@gmail.com>
2022-01-04 15:34:26 +0100

    add GetOrderbook API

--- responses.go
+++ responses.go
@@ -364,3 +364,88 @@ type InstrumentResponse struct {
 		Updated string `json:"updated"`
 	} `json:"companyOwners"`
 }
+
+type OrderbookResponse struct {
+	Customer struct {
+		ShowCourtageClassInfoOnOrderPage bool   `json:"showCourtageClassInfoOnOrderPage"`
+		CourtageClass                    string `json:"courtageClass"`
+	} `json:"customer"`
+	Account struct {
+		Type         string  `json:"type"`
+		TotalBalance float64 `json:"totalBalance"`
+		BuyingPower  float64 `json:"buyingPower"`
+		Name         string  `json:"name"`
+		ID           string  `json:"id"`
+	} `json:"account"`
+	Orderbook struct {
+		BuyPrice          float64 `json:"buyPrice"`
+		SellPrice         float64 `json:"sellPrice"`
+		Spread            float64 `json:"spread"`
+		HighestPrice      float64 `json:"highestPrice"`
+		LowestPrice       float64 `json:"lowestPrice"`
+		LastPrice         float64 `json:"lastPrice"`
+		LastPriceUpdated  string  `json:"lastPriceUpdated"`
+		Change            float64 `json:"change"`
+		ChangePercent     float64 `json:"changePercent"`
+		TotalVolumeTraded int     `json:"totalVolumeTraded"`
+		TotalValueTraded  float64 `json:"totalValueTraded"`
+		ExchangeRate      float64 `json:"exchangeRate"`
+		PositionVolume    int     `json:"positionVolume"`
+		Currency          string  `json:"currency"`
+		Tradable          bool    `json:"tradable"`
+		TradingUnit       int     `json:"tradingUnit"`
+		TickerSymbol      string  `json:"tickerSymbol"`
+		FlagCode          string  `json:"flagCode"`
+		VolumeFactor      float64 `json:"volumeFactor"`
+		Name              string  `json:"name"`
+		ID                string  `json:"id"`
+		Type              string  `json:"type"`
+	} `json:"orderbook"`
+	FirstTradableDate string   `json:"firstTradableDate"`
+	LastTradableDate  string   `json:"lastTradableDate"`
+	UntradableDates   []string `json:"untradableDates"`
+	OrderDepthLevels  []struct {
+		Buy struct {
+			Percent float64 `json:"percent"`
+			Price   float64 `json:"price"`
+			Volume  int     `json:"volume"`
+		} `json:"buy"`
+		Sell struct {
+			Percent float64 `json:"percent"`
+			Price   float64 `json:"price"`
+			Volume  int     `json:"volume"`
+		} `json:"sell"`
+	} `json:"orderDepthLevels"`
+	MarketMakerExpected    bool   `json:"marketMakerExpected"`
+	OrderDepthReceivedTime string `json:"orderDepthReceivedTime"`
+	LatestTrades           []struct {
+		Cancelled       bool    `json:"cancelled"`
+		Buyer           string  `json:"buyer"`
+		Seller          string  `json:"seller"`
+		MatchedOnMarket bool    `json:"matchedOnMarket"`
+		Price           float64 `json:"price"`
+		Volume          int     `json:"volume"`
+		DealTime        string  `json:"dealTime"`
+	} `json:"latestTrades"`
+	MarketTrades           bool `json:"marketTrades"`
+	HasShortSellKnowledge  bool `json:"hasShortSellKnowledge"`
+	HasInstrumentKnowledge bool `json:"hasInstrumentKnowledge"`
+	BrokerTradeSummary     struct {
+		OrderbookID string `json:"orderbookId"`
+		Items       []struct {
+			NetBuyVolume int    `json:"netBuyVolume"`
+			BuyVolume    int    `json:"buyVolume"`
+			SellVolume   int    `json:"sellVolume"`
+			BrokerCode   string `json:"brokerCode"`
+		} `json:"items"`
+	} `json:"brokerTradeSummary"`
+	HasInvestmentFees struct {
+		Buy  bool `json:"buy"`
+		Sell bool `json:"sell"`
+	} `json:"hasInvestmentFees"`
+	TickSizeRules []struct {
+		MinPrice float64     `json:"minPrice"`
+		MaxPrice float64 `json:"maxPrice"`
+		TickSize float64 `json:"tickSize"`
+	} `json:"tickSizeRules"`
+}
